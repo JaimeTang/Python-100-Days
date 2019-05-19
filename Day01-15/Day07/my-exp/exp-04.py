3e67af8c2d0eadde5de6deb0862933795594ee36
唐进民（Guilin Tang） <tjmts@qq.com>
2019-05-19 23:02:49 +0800

    更新部分内容

--- Day01-15/Day07/my-exp/exp-04.py
+++ Day01-15/Day07/my-exp/exp-04.py
@@ -7,7 +7,17 @@ Date: 2019-05-18
 """
 
 def which_day(year, month, day):
-    pass
+    days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if (year%4==0 and year%100 != 0) or year%400 == 0 :
+        days[1] = 29
+        whichday = days[:month-1]
+        whichday = sum(whichday)+day
+    
+    else:
+        whichday = days[:month-1]
+        whichday = sum(whichday)+day
+    
+    return whichday
 
 def main():
     print(which_day(1980, 11, 28))
